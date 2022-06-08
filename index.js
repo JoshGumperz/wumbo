9efc1f37029c12a8c45dde1f09d83d7b1f93f0e1
Josh Gumperz <joshgumperz@Joshs-MacBook-Pro.local>
2022-06-08 16:29:12 -0700

    initialize react app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const routes = require('./routes');
 
 dotenv.config()
