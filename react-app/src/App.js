543bfcc1f0f05734f42ae54777ca601dc014b696
Josh Gumperz <joshgumperz@Joshs-MacBook-Pro.local>
2022-06-08 17:29:44 -0700

    created some initial routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,24 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Home from './pages/home'
+import Login from './pages/login'
+import Signup from "./pages/signup";
 
 
 function App() {
   return (
-    <div>Hello!</div>
+    <Router>
+      <Switch>
+        <Route exact path={'/'}>
+          <Home/>
+        </Route>
+        <Route exact path={'/login'}>
+          <Login/>
+        </Route>
+        <Route exact path={'/signup'}>
+          <Signup/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
